dd588661738218bac5ccb247fd02d95fb92557cf
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-10-13 22:16:03 +0200

    Schadcn set up

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const IssuesPage = () => {
   return (
     <div>
-        Issues Page
+        <Button>New Issue</Button>
       
     </div>
   )
