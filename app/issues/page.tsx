c66010c610317c6254880072fb7ba52bfa0c12f8
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-10-14 21:58:03 +0200

    Issue table added

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,48 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import prisma from "@/prisma/client";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
   return (
-    <div className='mt-5 ml-2'>
-        <Button><Link href='/issues/new'>New Issue</Link></Button>
-      
-    </div>
-  )
-}
+    <div className="mt-5 ml-2">
+      <div className="mb-3">
+        <Button>
+          <Link href="/issues/new">New Issue</Link>
+        </Button>
+      </div>
 
-export default IssuesPage
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Title</TableHead>
+            <TableHead className="hidden md:table-cell">Status</TableHead>
+            <TableHead className="hidden md:table-cell">Date</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {issues.map((issue) => (
+            <TableRow key={issue.id}>
+              <TableCell className="font-medium">{issue.title}
+                <div className="block md:hidden">{issue.status}</div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">{issue.status}</TableCell>
+              <TableCell className="hidden md:table-cell">{issue.createdAt.toDateString()}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default IssuesPage;
