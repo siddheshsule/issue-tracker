4c964d3b8e1e44f16f8e5bd522b5bf480ed24680
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-10-14 19:05:04 +0200

    Error handling and toaster notification added

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,37 +1,57 @@
-'use client'
+"use client";
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
-import axios from "axios"
-import { useRouter } from 'next/navigation';
-
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/hooks/use-toast";
+import { useState } from "react";
 
 interface IssueForm {
-    title: string,
-    description : string
+  title: string;
+  description: string;
 }
 
 const NewIssuePage = () => {
-    const {register, control,  handleSubmit} = useForm<IssueForm>();
-    const router = useRouter();
-    const onSubmit = handleSubmit(async (data) => {
-        await axios.post('/api/issues', data);
-        router.push('/issues');
-    });
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      const errorMessage = axios.isAxiosError(error)
+        ? error.response?.data?.message ||
+          "Hmmm...something does'nt seem right! Please try after sometime."
+        : "An unexpected error occured.";
+      toast({
+        title: "Error!",
+        description: errorMessage,
+      });
+    }
+  });
 
   return (
-    <form className='max-w-xl p-5 space-y-3' onSubmit={onSubmit}>
-        <h2 className='text-xl font-semibold'>Create New Issue</h2>
-        <Input placeholder='Title' {...register('title')}/>
-        <Controller name='description' control={control} 
-        render={({field})=><SimpleMDE {...field} placeholder='Description' />}/>        
-        <Button type='submit'>Submit Issue</Button>
-      
+    <form className="max-w-xl p-5 space-y-3" onSubmit={onSubmit}>
+      <h2 className="text-xl font-semibold">Create New Issue</h2>
+      <Input placeholder="Title" {...register("title")} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE {...field} placeholder="Description" />
+        )}
+      />
+      <Button type="submit">Submit Issue</Button>
+      <Toaster />
     </form>
-  )
-}
+  );
+};
 
-export default NewIssuePage
+export default NewIssuePage;
